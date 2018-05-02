c946b4c7db874cc257046c9cb94f9edd11b43c21
lidyang <lidgyang@foxmail.com>
2018-05-03 01:00:28 +0800

    Reset Package

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,7 +1,8 @@
-package android.coolweather.com.coolweather;
+package com.coolweather.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.coolweather.com.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
